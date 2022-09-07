a12747e6ff0e20fd8ffd73cd220360b4f04a3a86
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-07 16:08:13 +0800

    格式化

--- search-bounds.d.ts
+++ search-bounds.d.ts
@@ -1,11 +1,41 @@
 //declare module 'binary-search-bounds' {
-    interface BSearch {
-        gt<T>(array:T[], y:T, compare?:((a:T, b:T) => number | null | undefined), lo?:number, hi?:number):number;
-        ge<T>(array:T[], y:T, compare?:((a:T, b:T) => number | null | undefined), lo?:number, hi?:number):number;
-        lt<T>(array:T[], y:T, compare?:((a:T, b:T) => number | null | undefined), lo?:number, hi?:number):number;
-        le<T>(array:T[], y:T, compare?:((a:T, b:T) => number | null | undefined), lo?:number, hi?:number):number;
-        eq<T>(array:T[], y:T, compare?:((a:T, b:T) => number | null | undefined), lo?:number, hi?:number):number;
-    }
-    const bsearch:BSearch;
-    export = bsearch;
+interface BSearch {
+    gt<T>(
+        array: T[],
+        y: T,
+        compare?: (a: T, b: T) => number | null | undefined,
+        lo?: number,
+        hi?: number
+    ): number;
+    ge<T>(
+        array: T[],
+        y: T,
+        compare?: (a: T, b: T) => number | null | undefined,
+        lo?: number,
+        hi?: number
+    ): number;
+    lt<T>(
+        array: T[],
+        y: T,
+        compare?: (a: T, b: T) => number | null | undefined,
+        lo?: number,
+        hi?: number
+    ): number;
+    le<T>(
+        array: T[],
+        y: T,
+        compare?: (a: T, b: T) => number | null | undefined,
+        lo?: number,
+        hi?: number
+    ): number;
+    eq<T>(
+        array: T[],
+        y: T,
+        compare?: (a: T, b: T) => number | null | undefined,
+        lo?: number,
+        hi?: number
+    ): number;
+}
+declare const bsearch: BSearch;
+export = bsearch;
 //}
